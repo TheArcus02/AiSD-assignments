d00e9ff6542cf29004ee1fd1b789e57df0ea8f30
TheArcus02 <m.urbaniak.inf@gmail.com>
2023-05-23 20:04:12 +0200

    added knapsack algorithms

---
+++ knapsack/knapsack.py
@@ -0,0 +1,31 @@
+def knapSackDynamic(max_weight, weights, values, num_items):
+
+    dp = [0 for _ in range(max_weight + 1)]
+
+    for i in range(1, num_items + 1):
+
+        for current_weight in range(max_weight, 0, -1):
+            if weights[i-1] <= current_weight:
+
+                dp[current_weight] = max(
+                    dp[current_weight], dp[current_weight - weights[i-1]] + values[i-1])
+    return dp[max_weight]
+
+
+def knapSackGreedy(max_weight, weights, values):
+    items = list(zip(weights, values))
+    items.sort(key=lambda x: x[1]/x[0], reverse=True)
+
+    total_value = 0
+    remaining_weight = max_weight
+
+    for item in items:
+        weight = item[0]
+        value = item[1]
+
+        if remaining_weight >= weight:
+            # If the item can be accommodated in the knapsack, add it
+            total_value += value
+            remaining_weight -= weight
+
+    return total_value
