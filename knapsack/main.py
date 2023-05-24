668e8351e01ae0fe971abc0d507a77dd6decbccd
TheArcus02 <m.urbaniak.inf@gmail.com>
2023-05-24 12:09:31 +0200

    added functions to measure algorithms and generate graph

--- knapsack/main.py
+++ knapsack/main.py
@@ -5,9 +5,16 @@ import matplotlib.pyplot as plt
 
 
 def main():
+    MAX_WEIGHT = 100
+    MAX_ITEMS = 30_000
+    STEP = 1000
 
-    constMaxWeight(100, 1000, 100)
-    generate_plot(log=False)
+    '''
+        Uncomment the function you want to run
+        and generate graph from it
+    '''
+    constMaxWeight(MAX_WEIGHT, MAX_ITEMS, STEP)
+    # constItems(MAX_WEIGHT, MAX_ITEMS, STEP)
 
 
 def constMaxWeight(max_weight: int, max_items: int, step: int):
@@ -15,12 +22,14 @@ def constMaxWeight(max_weight: int, max_items: int, step: int):
     times = []
     solves = [[], []]
     solve_quality = []
-    lengths = range(10, max_items, step)
+    lengths = range(1, max_items, step)
     algorithms = [knapSackDynamic, knapSackGreedy]
 
     figure, axis = plt.subplots(1, 2)
-    axis[0].set_title(f'Execution Time')
+    axis[0].set_title(f'Execution Time (logaritmic scale)')
+    axis[0].set_xlabel(f'List Length')
     axis[1].set_title(f'Solve Quality')
+    axis[1].set_xlabel(f'List Length')
 
     for idx, algorithm in enumerate(algorithms):
         for data_length in lengths:
@@ -41,15 +50,53 @@ def constMaxWeight(max_weight: int, max_items: int, step: int):
         solve_quality.append(solves[0][idx] - solves[1][idx] / solves[0][idx])
     axis[1].plot(lengths, solve_quality, label='Solve Quality')
 
+    # logaritmic scale for execution time
+    axis[0].set_yscale('log')
+
     axis[0].legend()
     axis[1].legend()
+    plt.show()
 
 
-def generate_plot(log=False):
-    plt.style.use("dark_background")
-    plt.xlabel("List Length")
-    if log:
-        plt.yscale('log')
+def constItems(max_weight: int, max_items: int, step: int):
+
+    times = []
+    solves = [[], []]
+    solve_quality = []
+    max_weights = range(1, max_weight, step)
+    algorithms = [knapSackDynamic, knapSackGreedy]
+
+    figure, axis = plt.subplots(1, 2)
+    axis[0].set_title(f'Execution Time (logaritmic scale)')
+    axis[0].set_xlabel(f'Max Weight')
+    axis[1].set_title(f'Solve Quality')
+    axis[1].set_xlabel(f'Max Weight')
+
+    for idx, algorithm in enumerate(algorithms):
+        for current_weight in max_weights:
+
+            weights = [random.randint(1, current_weight)
+                       for _ in range(max_items)]
+            values = [random.randint(1, 1000) for _ in range(max_items)]
+
+            start = time.perf_counter()
+            result = algorithm(current_weight, weights, values, max_items)
+            stop = time.perf_counter()
+
+            solves[idx].append(result)
+            times.append(stop - start)
+        axis[0].plot(max_weights, times, label=f'{algorithm.__name__}')
+        times = []
+
+    for idx in range(len(solves[0])):
+        solve_quality.append(solves[0][idx] - solves[1][idx] / solves[0][idx])
+    axis[1].plot(max_weights, solve_quality, label='Solve Quality')
+
+    # logaritmic scale for execution time
+    axis[0].set_yscale('log')
+
+    axis[0].legend()
+    axis[1].legend()
     plt.show()
 
 
